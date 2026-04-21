899a5ae421546989ec57e4e83ffd2441c6cb0757
ays19 <sahsanyasir@gmail.com>
2026-04-21 10:03:31 +0600

    Add Workouts model and initial migrations

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class workouts(models.Model):
+    activity = models.CharField(max_length=200)
+    duration = models.IntegerField(help_text="Duration in minutes")
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.activity} - {self.duration} min on {self.date}"
