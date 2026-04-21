03433a8be182d3aa1e441489a4abda1b4fbe4ac6
ays19 <sahsanyasir@gmail.com>
2026-04-21 15:03:14 +0600

    Add view logic for creating new workouts

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Workout
+from .forms import WorkoutForm
 
-# Create your views here.
+# VIEW TO LIST WORKOUTS
+def workout_list(request)
+    workout= Workout.objects.all().order_by('-date')
+    return render(request, 'tracker/workout_list.html', {'workouts': workout})
+
+# view to add a workout
+def add_workout(request):
+    if request.method == 'POST':
+        form = WorkoutForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('workout_list')
+        else:
+            form = Workout()
+        return render(request, 'tracker/add_workout.html',{'form': form})
