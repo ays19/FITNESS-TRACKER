2722177fad99f97652cc5506959472674bff9c87
ays19 <sahsanyasir@gmail.com>
2026-04-22 22:06:32 +0600

    runserver and project worked perfectly

--- tracker/views.py
+++ tracker/views.py
@@ -4,16 +4,19 @@ from .forms import WorkoutForm
 
 # VIEW TO LIST WORKOUTS
 def workout_list(request):
-    workout= Workout.objects.all().order_by('-date')
-    return render(request, 'tracker/workout_list.html', {'workouts': workout})
+    workouts= Workout.objects.all().order_by('-date')
+    return render(request, 'tracker/workout_list.html', {'workouts': workouts})
 
 # view to add a workout
 def add_workout(request):
     if request.method == 'POST':
         form = WorkoutForm(request.POST)
+        print("POST data:", request.POST)       # check data is coming in
+        print("Form valid:", form.is_valid())    # check if valid
+        print("Form errors:", form.errors) 
         if form.is_valid():
             form.save()
             return redirect('workout_list')
-        else:
-            form = Workout()
-        return render(request, 'tracker/add_workout.html',{'form': form})
+    else:
+        form = WorkoutForm()
+    return render(request, 'tracker/add_workout.html',{'form': form})
